cd8423a384dadb2f750b0211fb365f953896896b
lucascott <lucascott96@hotmail.it>
2017-05-02 18:30:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,20 +4,79 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <signal.h>
 
 #define MAX_ARGS 256
 #define MAX_LINE_SIZE 1024
 
-pid_t pid;
-static char line[1024];
-char * pch[MAX_ARGS];
+pid_t   pid;
+char    line[1024];
+char    *pch[MAX_ARGS];
 
-void esegui(char * words[MAX_ARGS], int arg_counter);
+pid_t   processi[100];
+int     pcount = 0;
+
+/// FUNZIONI
+
+void ChildProcess(char *n)
+{
+     pid_t  pid;
+     char   *name = n;
+
+     pid = getpid();
+     printf("Child process %s starts (pid = %d)\n", name, pid);
+     wait();
+}
+
+void esegui(char *words[MAX_ARGS], int arg_counter) {
+    if (arg_counter == 1 && strcmp(words[0],"phelp") == 0){
+        printf("\nComandi disponibili:\nphelp​ : stampa un elenco dei comandi disponibili\nplist​ : elenca i processi generati dalla shell custom\npnew <nome>​ : crea un nuovo processo con nome <nome>\npinfo <nome>​ : fornisce informazioni sul processo <nome> (almeno ​ pid ​ e ​ ppid ​ )\npclose <nome>​ : chiede al processo <nome> di chiudersi\nquit​ : esce dalla shell custom\n\n");
+    }
+    else if (arg_counter == 1 && strcmp(words[0],"plist") == 0){
+        printf("%d processi figli attivi\n", pcount);
+        int i = 0;
+        while (i < pcount) {
+            printf("Open process with pid = %d\n",processi[i]);
+            i++;
+        }
+
+    }
+    else if (arg_counter == 2 && strcmp(words[0],"pnew") == 0){
+        printf("Creazione del processo %s\n", words[1]);
+        pid_t p
+        processi[pcount] = fork();
+        if (processi[pcount] < 0) {
+            printf("Failed to fork process\n");
+            exit(1);
+        }
+        else if (processi[pcount] == 0){
+            pcount++; // Nb da tenere sempre dopo l'if ma prima del ChildProcess
+            ChildProcess(words[1]);
+        }
+    }
+    else if (arg_counter == 2 && strcmp(words[0],"pinfo") == 0){
+        printf("informazioni sul processo %s (almeno pid e ppid)\n", words[1]);
+    }
+    else if (arg_counter == 2 && strcmp(words[0],"pclose") == 0){
+        printf("chiede al processo %s di chiudersi\n", words[1]);
+        kill(, SIGKILL);
+    }
+    else if (arg_counter == 1 && strcmp(words[0],"quit") == 0)
+        exit(0);
+}
 
 int main()
 {
     printf("SIMPLE SHELL: Type 'exit' or send EOF to exit.\n");
     int argc = 0;
+
+    // DEBUG
+    pid_t main_p = getpid();
+    printf("Main process id = %d\n", main_p);
+    // end debug
+
+    
     while (1) {
         /* Print the command prompt */
         printf("$> ");
@@ -30,24 +89,12 @@ int main()
             pch[argc] = strtok(line," ,.-\n\t");
             while (pch[argc] != NULL) {
                 argc++;
-                printf ("%s\n",pch[argc-1]);
+                //printf ("%s\n",pch[argc-1]);
                 pch[argc] = strtok (NULL, " ,.-\n\t");
             }
         }
-        printf ("END, words: %d\n", argc);
+        //printf ("END, words: %d\n", argc);
         esegui(pch, argc);
     }
     return 0;
-}
-
-void esegui(char * words[MAX_ARGS], int arg_counter) {
-    //printf("%d\n", arg_counter);
-    if (arg_counter == 1 && strcmp(words[0],"phelp") == 0){
-        printf("\nComandi disponibili:\nphelp​ : stampa un elenco dei comandi disponibili\nplist​ : elenca i processi generati dalla shell custom\npnew <nome>​ : crea un nuovo processo con nome <nome>\npinfo <nome>​ : fornisce informazioni sul processo <nome> (almeno ​ pid ​ e ​ ppid ​ )\npclose <nome>​ : chiede al processo <nome> di chiudersi\nquit​ : esce dalla shell custom\n\n");
-    }/*
-    else if (strcmp("phelp",primo) == 0 && arg_counter > 1)
-        printf("Sei mona, come delsi\n");
-    */
-    else if (arg_counter == 1 && strcmp(words[0],"quit") == 0)
-        exit(0);
 }
